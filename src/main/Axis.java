22cb32cd0e4f08f6b66bcad06b052858767833e0
Yuri-Gabriel <yurigabriel.f1012@gmail.com>
2025-02-24 14:27:20 -0300

    sei la

--- src/main/Axis.java
+++ src/main/Axis.java
@@ -8,19 +8,46 @@ import javax.swing.JPanel;
 public class Axis extends JPanel {
 	
 	public void paintComponent(Graphics g) {
+		int window_width = Window.getFrames()[0].getWidth();
+		int window_height = Window.getFrames()[0].getHeight();
+		
+		int point_x_axis_distance = window_width / 10;
+		int point_y_axis_distance = 100;
+		
 		g.setColor(Color.BLACK);
+		
+		// x axis
 		g.drawLine(
 				0,
-				Window.getFrames()[0].getHeight()/2,
-				Window.getFrames()[0].getWidth(),
-				Window.getFrames()[0].getHeight()/2
+				window_height/2,
+				window_width,
+				window_height/2
 		);
 		
+		for(int i = window_width * -1; i <= window_width; i += point_x_axis_distance) {
+			g.drawLine(
+					i,
+					window_height/2-5,
+					i,
+					window_height/2+5
+			);
+		}
+		
+		// y axis
 		g.drawLine(
-				Window.getFrames()[0].getWidth()/2,
+				window_width/2,
 				0,
-				Window.getFrames()[0].getWidth()/2,
-				Window.getFrames()[0].getHeight()
+				window_width/2,
+				window_height
 		);
+		
+		for(int i = window_height * -1; i <= window_height; i += point_y_axis_distance) {
+			g.drawLine(
+					window_width/2-5,
+					i,
+					window_width/2+5,
+					i
+			);
+		}
 	}
 }
