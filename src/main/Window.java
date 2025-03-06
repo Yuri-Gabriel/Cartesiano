69106a6204e05ad12a844fa0c809243fbd211d90
Yuri-Gabriel <yurigabriel.f1012@gmail.com>
2025-03-06 10:21:33 -0300

    função f(x) ta horrivel

--- src/main/Window.java
+++ src/main/Window.java
@@ -5,6 +5,10 @@ import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ContainerEvent;
 import java.awt.event.ContainerListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -14,10 +18,11 @@ public class Window extends JFrame {
 	private Axis axis;
 	public final static int HEIGHT = 600;
 	public final static int WIDTH = 600;
+	public static double WINDOW_SCALE = 1;
 	
 	public Window() {
 		this.setSize(HEIGHT, WIDTH);
-		this.setResizable(true);
+		this.setResizable(false);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setBackground(Color.WHITE);
 		
@@ -38,6 +43,28 @@ public class Window extends JFrame {
                 System.out.println("h: " + window_height);
             }
         });
+		
+		
+		this.addMouseWheelListener(new MouseWheelListener() {
+			
+			@Override
+			public void mouseWheelMoved(MouseWheelEvent wheel_event) {
+				// TODO Auto-generated method stub
+				
+				double wheel_rotation = wheel_event.getPreciseWheelRotation();
+				if(wheel_rotation > 0) {
+					// para baixo
+					if(WINDOW_SCALE > 0.2) WINDOW_SCALE -= 0.1;
+				}
+				if(wheel_rotation < 0) {
+					// para cima
+					
+					if(WINDOW_SCALE <= 2.5) WINDOW_SCALE += 0.1;
+				}
+				axis.repaint();
+			}
+		});
+		
 		
 		this.setVisible(true);
 	}
